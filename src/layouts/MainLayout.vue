e95da2899825dc8282ff95750383f3631075a33e
siphera <salmansiphenkosi@gmail.com>
2021-04-30 16:13:50 +0200

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,58 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="left = !left" />
+
+        <q-toolbar-title class="text-weight-bold">
+          Qwitter
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer :width="283" show-if-above v-model="left" side="left" bordered>
+      <q-icon class="q-pa-md" name="fas fa-dove" size="lg" color="primary" />
+      <q-list >
+      <q-item to="/" clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="home" size="md" />
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+      </q-item>
+      <q-item to="/about" clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon name="help" size="md" />
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">About</q-item-section>
+      </q-item>
+    </q-list>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="right" side="right" bordered>
+      <q-input square filled >
+        <template v-slot:prepend>
+          <q-icon name="event" />
+        </template>
+      </q-input>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      left: false,
+      right: false
+    }
+  }
+}
+</script>
